944dc64524c377fd5bef7677ba74e26b8b616c99
GWeizman <gweizman@gmail.com>
2015-05-01 01:55:59 +0300

    callbacks

--- js/buildToar.js
+++ js/buildToar.js
@@ -4,20 +4,19 @@ $(function() {
             $("#faculty > #buttons").append('<a data-id="' + faculty.id + '" class="button">' + faculty.name + '</a>');
             $("#faculty > #buttons > a[data-id=\"" + faculty.id + "\"]").data("object", faculty);
         });
-    });
-    
-    $("#faculty > .button").click(function() {
-        $("#chug-faculty").html($(this).html(), function() {
-            var _this = $(this);
-            console.log(_this.data("object"));
-            _this.data("object").fetchChugim(function() {
-                console.log(_this.data("object").getChugim());
-                _this.data("object").getChugim().forEach(function(chug) {
-                    $("#chug > #buttons").append('<a data-id="' + chug.id + '" class="button">' + chug.name + '</a>');
-                    $("#chug > #buttons > a[data-id=\"" + chug.id + "\"]").data("object", chug);                   
-                });
-                $("#faculty").hide("slide", function() {
-                        $("#chug").show("slide");
+        $("#faculty > .button").click(function() {
+            $("#chug-faculty").html($(this).html(), function() {
+                var _this = this;
+                //console.log(_this.data("object"));
+                $(_this).data("object").fetchChugim(function() {
+                    //console.log(_this.data("object").getChugim());
+                    $(_this).data("object").getChugim().forEach(function(chug) {
+                        $("#chug > #buttons").append('<a data-id="' + chug.id + '" class="button">' + chug.name + '</a>');
+                        $("#chug > #buttons > a[data-id=\"" + chug.id + "\"]").data("object", chug);                   
+                    });
+                    $("#faculty").hide("slide", function() {
+                            $("#chug").show("slide");
+                    });
                 });
             });
         });
