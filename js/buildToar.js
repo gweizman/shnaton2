b304f994688fc8781f1154d0ddb09b5227b205ca
GWeizman <gweizman@gmail.com>
2015-05-01 01:49:56 +0300

    chugim

--- js/buildToar.js
+++ js/buildToar.js
@@ -2,14 +2,22 @@ $(function() {
     populateFaculties(function(faculties) {
         faculties.forEach(function(faculty) {
             $("#faculty > #buttons").append('<a data-id="' + faculty.id + '" class="button">' + faculty.name + '</a>');
-            $("a[data-id=\"" + faculty.id + "\"]").data("object", faculty);
+            $("#faculty > #buttons > a[data-id=\"" + faculty.id + "\"]").data("object", faculty);
         });
     });
     
     $("#faculty > .button").click(function() {
-        $("#chug-faculty").html($(this).html());
-        $("#faculty").hide("slide");
-        $("#chug").show("slide");
+        $("#chug-faculty").html($(this).html(), function() {
+            $(this).data("object").fetchChugim(function() {
+                $(this).data("object").getChugim.forEach(function(chug) {
+                    $("#chug > #buttons").append('<a data-id="' + chug.id + '" class="button">' + chug.name + '</a>');
+                    $("#chug > #buttons > a[data-id=\"" + chug.id + "\"]").data("object", chug);                   
+                });
+                $("#faculty").hide("slide", function() {
+                        $("#chug").show("slide");
+                });
+            });
+        });
     });
     
     $("#chug > .button").click(function() {
