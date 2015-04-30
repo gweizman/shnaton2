07a4c6b34450f7ba545b45b550135dfb1dfb8dd8
GWeizman <gweizman@gmail.com>
2015-05-01 01:25:36 +0300

    afwaef

--- js/app.js
+++ js/app.js
@@ -10,18 +10,18 @@ function loadPage(html, javascript) {
 
 function loadCourse(id) {
     console.log(id);
-    $('#courseContent').load('/showCourse.html?id=' + id, function() {
+    $('#courseContent').load('showCourse.html?id=' + id, function() {
         $('#courseModal').foundation('reveal', 'open');
     });
 }
 
 function loadMainPage() {
     if (typeChosen == null) {
-        loadPage("/buildToar.html", "/js/buildToar.js");
+        loadPage("buildToar.html", "js/buildToar.js");
     } else {
         switch (typeChosen) {
             case "exists":
-                loadPage("/timeline.html", "/js/timeline.js");
+                loadPage("timeline.html", "js/timeline.js");
                 break;
             case "new":
             default:
